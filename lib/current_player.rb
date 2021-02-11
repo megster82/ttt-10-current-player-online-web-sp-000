915e2e90079de8746b02feec0a111e16367b7f62
meaghan jennings <meaghanljennings@gmail.com>
2021-02-11 14:25:49 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,18 +1,12 @@
 def turn_count(board)
-  turns = 0
-  board.each do |space|
-    if space == "X" || space == "O"
-      turns += 1
-    end
+  counter = 
+  if #my condition is met 
+    counter +=  1
   end
-  return turns
-end
   
+end
+
 def current_player(board)
-  turns = turn_count(board)
-  if turns % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
+  
+  
 end
